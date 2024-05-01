cb167b316b1acac5f44fe609c520113c13e715ce
Ushakov Oleg <Ushakov_o@gmail.com>
2024-05-01 14:43:01 +0700

    FTT-0 added SpringBoot to the project.

---
+++ src/main/java/com/github/olegushak/FTT/FlightTicketsTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.olegushak.FTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketsTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketsTelegrambotApplication.class, args);
+	}
+
+}
