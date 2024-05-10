a60cf727a3ef2b6fe275ab5983c2f583685766cc
Ushakov Oleg <Ushakov_o@gmail.com>
2024-05-10 12:03:07 +0700

    FTT-3: commented unimplemented commands.

--- src/main/java/com/github/olegushak/FTT/command/CommandName.java
+++ src/main/java/com/github/olegushak/FTT/command/CommandName.java
@@ -3,12 +3,12 @@ package com.github.olegushak.FTT.command;
 public enum CommandName {
     START("/start"),
     HELP("/help"),
-    FIND_FLIGHT("найти билет"),
-    FOLLOW_FLIGHT("отслеживать билет"),
-    NO("nocommand"),
-    FLIGHT_HISTORY("завершенные перелеты"),
-    ALL_FLIGHTS("мои перелеты"),
-    MAIN("главное меню");
+   // FIND_FLIGHT("найти билет"),
+   // FOLLOW_FLIGHT("отслеживать билет"),
+    NO("nocommand");
+   // FLIGHT_HISTORY("завершенные перелеты"),
+  //  ALL_FLIGHTS("мои перелеты"),
+  //  MAIN("главное меню");
 
     private final String commandName;
 
